78ec2ca7b0e89fc76c139fc947026d439ea202cd
filipesousaist <filipe.miguel.sousa@tecnico.ulisboa.pt>
2022-05-10 17:09:10 +0100

    Added option to run without extra terminal

--- evaluate1v0.py
+++ evaluate1v0.py
@@ -5,6 +5,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("agent", choices=["npc", "drqn"])
     parser.add_argument("-d", "--display", action="store_true")
+    parser.add_argument("-t", "--terminal", action="store_true")
     parser.add_argument("-l", "--load", type=str)
     args = parser.parse_args()
     
@@ -14,7 +15,8 @@ def main():
 
     if args.agent == "drqn":
         loadArg = " --load " + args.load if args.load else ""
-        os.system("gnome-terminal -x script -c 'python drqn-offense-agent-for-1v0.py{}' output/output.txt".format(loadArg))
+        terminalCommand = "gnome-terminal -x " if args.terminal else ""
+        os.system("{}script -c 'python drqn-offense-agent-for-1v0.py{}' output/output.txt".format(terminalCommand, loadArg))
     
     while not input().lower().startswith('q'):
         pass
