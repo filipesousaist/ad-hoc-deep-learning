3b841a1a7637282252361c5b2140d87e35b7a9c4
filipesousaist <filipe.miguel.sousa@tecnico.ulisboa.pt>
2022-05-10 16:55:47 +0100

    Experiment 6

--- evaluate1v0.py
+++ evaluate1v0.py
@@ -5,6 +5,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("agent", choices=["npc", "drqn"])
     parser.add_argument("-d", "--display", action="store_true")
+    parser.add_argument("-l", "--load", type=str)
     args = parser.parse_args()
     
     playersArg = "--offense-agents" if args.agent == "drqn" else "--offense-npcs" 
@@ -12,7 +13,8 @@ def main():
     os.system("LC_ALL=C ../HFO/bin/HFO {} 1 {} &".format(playersArg, displayArg))
 
     if args.agent == "drqn":
-        os.system("gnome-terminal -x script -c 'python drqn-offense-agent-for-1v0.py' output/output.txt")
+        loadArg = " --load " + args.load if args.load else ""
+        os.system("gnome-terminal -x script -c 'python drqn-offense-agent-for-1v0.py{}' output/output.txt".format(loadArg))
     
     while not input().lower().startswith('q'):
         pass
