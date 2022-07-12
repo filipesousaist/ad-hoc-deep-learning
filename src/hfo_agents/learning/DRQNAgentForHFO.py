9912e2dbdf371dbc1351dcfafe2a6cb41eb34d96
filipesousaist <filipe.miguel.sousa@tecnico.ulisboa.pt>
2022-07-12 17:15:21 +0100

    preparations for experiments 39.4 and 40.3

--- src/hfo_agents/learning/DRQNAgentForHFO.py
+++ src/hfo_agents/learning/DRQNAgentForHFO.py
@@ -5,4 +5,4 @@ from src.hfo_agents.learning.LearningAgentForHFO import LearningAgentForHFO
 
 class DRQNAgentForHFO(LearningAgentForHFO):
     def _createAgent(self, num_features: int, num_actions: int, parameters: dict) -> DRQNAgent:
-        return DRQNAgent(num_features, num_actions, **parameters)
+        return DRQNAgent(num_features, num_actions, **parameters, )
