adff1a9a32f7c1e7a84645f6c3544c47bf501036
filipesousaist <filipe.miguel.sousa@tecnico.ulisboa.pt>
2022-11-29 01:32:31 +0000

    list_experiments

--- src/run_seq.py
+++ src/run_seq.py
@@ -1,8 +1,9 @@
 import os
 import sys
+import re
 import argparse
 
-from src.lib.io import writeJSON, readJSON
+from src.lib.io import writeJSON, readJSON, readTxt
 from src.lib.paths import DEFAULT_PORT, DEFAULT_DIRECTORY, getPath
 from src.lib.io import printTable
 
@@ -16,6 +17,9 @@ def main():
     parser.add_argument("-p", "--port", type=str)
     parser.add_argument("-s", "--save-period", type=int)
     parser.add_argument("-i", "--info", action="store_true")
+    parser.add_argument("-e", "--experiments", type=str)
+    parser.add_argument("-S", "--status", type=str)
+    parser.add_argument("-R", "--regular-expression", type=str)
     parser.add_argument("-r", "--remove", type=str)
     args = parser.parse_args()
 
@@ -24,8 +28,21 @@ def main():
 
     ports = readJSON(PORTS_FILE)
 
+    if sum([bool(flag) for flag in (args.info, args.experiments, args.remove)]) > 1:
+        exit("At most one of --info, --remove and --experiments can be set.")
+
     if args.info:
         printTable([{"port": key, "program": ports[key]} for key in ports], ["port", "program"])
+    elif args.experiments:
+        experiments_list = []
+        getExperiments(args.experiments, experiments_list)
+        if args.status:
+            experiments_list = list(filter(lambda item: item["status"] == args.status, experiments_list))
+        if args.regular_expression:
+            pattern = re.compile(args.regular_expression)
+            experiments_list = list(filter(lambda item: re.match(pattern, item["path"]), experiments_list))
+        experiments_list = sorted(experiments_list, key=lambda item: item["path"])
+        printTable(experiments_list, ["path", "status"])
     elif args.remove:
         removePort(ports, args.remove)
     else:
@@ -53,6 +70,27 @@ def removePort(ports, port):
     writeJSON(PORTS_FILE, ports)
 
     print(f"Successfully removed port {port}")
+
+
+def getExperiments(directory, experiments_list):
+    files = os.listdir(directory)
+    if os.path.exists(getPath(directory, "input")):
+        status = "New"
+        save_path = getPath(directory, "save")
+        if os.path.exists(save_path):
+            if readTxt(save_path)["next_episode"] == "44050":
+                status = "Finished"
+            else:
+                status = "In Progress"
+        experiments_list.append({
+            "path": directory,
+            "status": status
+        })
+    for file in files:
+        path = os.path.join(directory, file)
+        if os.path.isdir(path):
+            getExperiments(path, experiments_list)
+
 
 
 if __name__ == "__main__":
